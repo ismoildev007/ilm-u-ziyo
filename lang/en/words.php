8cc549cfc1b340085bfa988e13c9a0de8cefb65c
ismoildev007 <ismoil3436@gmail.com>
2024-04-27 11:09:35 +0500

    fayl qo'shdim

---
+++ lang/en/words.php
@@ -0,0 +1,45 @@
+<?php
+return
+
+[
+    'about' => 'About us',
+    'home' => 'Home',
+    'educational' => 'Educational directions',
+    'teacher' => 'Teachers',
+    'student' => 'student life',
+    'sign_up' => 'Sign up',
+    'con' => 'contact',
+
+    'a' => 'Students',
+    'b' => 'Teachers',
+    'c' => 'Departments',
+    'd' => 'Practice Bases',
+
+
+    'card_a' => 'What do we offer you?',
+    'card_b' => 'Advanced Educational Programs',
+    'card_c' => 'Practice-based lessons',
+    'card_d' => 'Personal and professional development',
+
+    'ilm_u_ziyo' => 'medical college',
+    'text' => 'Ilm-u-Ziya Medical College is a first-class educational institution that aims to develop a wide range of skills in the field of modern nursing education and health care. In 2022, This college, which opened its doors, has a license issued by the State Inspectorate for Quality Control of Education',
+    'edu' => 'Educational programs',
+    'professor' => 'professor teachers',
+    'text_a' => 'Educational process at "Ilm-u-Ziya Medical College" is focused not only on imparting knowledge, but also on professional and personal growth of students. Each of our professors has extensive experience in their field of study and is ready to provide the best education and practice to our students.',
+    'photo' => 'Photos from student life',
+    'text_b' => 'Studying at "Ilm-u-Ziya Medical College" is not only an experience within the confines of classrooms, but an opportunity to apply practical knowledge and skills in real-world situations. Our students put theory to the test, giving them the real-world experience they need to succeed in the healthcare industry.',
+    'question' => 'Questions you may be interested in',
+    'text_c' => 'We have prepared answers to some questions that may be important to you in our college life. If your question is not on this list, please contact us, we will be happy to help you.',
+    'text_d' => 'Admission to the Medical College of Science and Technology is the first step in obtaining quality medical education and professional development.',
+    'sign_pass' => 'Documents required for registration:',
+    'pass' => 'Copy of valid passport or identity document.',
+    'diploma' => 'Original or certified copy of 11th grade certificate or college or high school diploma',
+    'contact' => 'Contact us:',
+    'location' => 'Address',
+    'location_a' => 'Kashkadarya region, Karshi sh. Makhallot neighborhood, Olimlar Street',
+    'number' => 'Phone number',
+    'partner_collage' => 'Partner Medical Colleges',
+    'partner_school' => 'Partner private schools',
+    'number_text' => 'If you want to get information about partner institutions or any other questions, you can call us at the following phone number:',
+    'internet' => 'social media pages:',
+];
---
+++ lang/uz/words.php
@@ -0,0 +1,45 @@
+<?php
+return
+
+    [
+        'about' => 'Biz haqimizda',
+        'home' => 'Bosh saxifa',
+        'educational' => 'Ta\'lim yo\'nalishlar',
+        'teacher' => 'O\'qituvchilar',
+        'student' => 'Talabalar hayoti',
+        'sign_up' => 'Ro\'yxatdan o\'tish',
+        'con' => 'Bog\'lanish',
+
+        'a' => 'Talabalar',
+        'b' => 'O\'qituvchilar',
+        'c' => 'Kafedralar',
+        'd' => 'Amaliyot Bazalar',
+
+
+        'card_a' => 'Biz sizga nima taklif qilamiz?',
+        'card_b' => 'Kengaytirilgan ta\'lim dasturlari',
+        'card_c' => 'Amaliyotga asoslangan darslar',
+        'card_d' => 'Shaxsiy va kasbiy rivojlanish',
+
+        'ilm_u_ziyo' => 'tibbiyot kolleji',
+        'text' => 'Ilm-u ziyo tibbiyot kolleji" - bu zamonaviy hamshiralik ta\'limi va sog\'liqni saqlash sohasida keng ko\'nikmalarni rivojlantirishni maqsad qilgan birinchi darajali ta\'lim muassasasidir. 2022-yilda o\'z eshiklarini ochgan bu kollej, "Ta\'lim sifatini nazorat qilish davlat inspeksiyasi" tomonidan berilgan',
+        'edu' => 'Ta\'lim dasturlari',
+        'professor' => 'professor o\'qituvchilari',
+        'text_a' => '"Ilm-u ziyo tibbiyot kolleji"da ta\'lim berish jarayoni nafaqat bilim berish, balki talabalarning kasbiy va shaxsiy o\'sishiga yo\'naltirilgan. Har bir professor-o\'qituvchimiz o\'z fan sohasida keng tajribaga ega bo\'lib, talabalarimizga eng yaxshi ta\'lim va amaliyotni taqdim etishga tayyordirlar.',
+        'photo' => 'Talabalar hayotidan foto lavhalar',
+        'text_b' => '"Ilm-u ziyo tibbiyot kolleji"da o\'qish - bu faqatgina dars xonalari chegarasidagi tajriba emas, balki amaliy bilim va mahoratni real dunyo sharoitida qo\'llash imkoniyatidir. Bizning talabalarimiz nazariyani amalda sinab ko\'rishadi, bu ularni sog\'liqni saqlash sohasida muvaffaqiyatli faoliyat yuritish uchun zarur bo\'lgan real tajribaga ega qiladi.',
+        'question' => 'Sizni qiziqtirishi mumkin bo\'lgan savollar',
+        'text_c' => 'Bizning kollej hayotida siz uchun muhim bo\'lishi mumkin bo\'lgan ba\'zi savollarga oldindan javoblar tayyorladik. Agar sizning savolingiz ushbu ro\'yxatda bo\'lmasa, iltimos biz bilan bog\'laning, biz sizga yordam berishdan mamnun bo\'lamiz.',
+        'text_d' => 'Ilm-u ziyo tibbiyot kollejiga kirish - bu sifatli tibbiy ta\'lim va professional rivojlanish imkoniyatiga ega bo\'lishning birinchi qadami.',
+        'sign_pass' => 'Ro\'yxatdan o\'tish uchun zarur hujjatlar:',
+        'pass' => 'Amaldagi pasport yoki shaxsni tasdiqlovchi hujjat nusxasi.',
+        'diplom' => '11-sinfni tamomlaganlik haqida attestat yoki kollej yoki litsey diplomining asli yoki tasdiqlangan nusxasi',
+        'contact' => 'Biz bilan bog\'lanish:',
+        'location' => 'Manzil',
+        'location_a' => 'Qashqadaryo viloyati, Qarshi sh. Maxallot mahallasi, Olimlar ko\'chasi',
+        'number' => 'Telefon nomer',
+        'partner_collage' => 'Hamkor tibbiyot texnikumlari',
+        'partner_school' => 'Hamkor xususiy maktablar',
+        'number_text' => 'Agar sizda hamkor muassasalar yoki boshqa har qanday savollar bo\'yicha ma\'lumot olishni istasangiz, bizga quyidagi telefon raqamidan qo\'ng\'iroq qilishingiz mumkin:',
+        'internet' => 'ijtimoiy tarmoqlari sahifalari:',
+    ];
